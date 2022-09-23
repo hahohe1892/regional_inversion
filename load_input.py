832e050258fff20467f12e268aa8437edffc876e
Thomas Frank <thomas.frank@geo.uu.se>
2022-09-23 15:16:30 +0200

    load dem

--- load_input.py
+++ load_input.py
@@ -4,6 +4,12 @@ import rioxarray as rioxr
 RID = 'RGI60-08.00001'
 def load_dhdt(RID):
     path = '/home/thomas/regional_inversion/input_data/dhdt/RGI60-08/RGI60-08.0'+RID[10] + '/'+RID+ '/dem.tif'
-    dem = rioxr.open_rasterio(path)
+    dhdt = rioxr.open_rasterio(path)
 
     return dhdt
+
+def load_dem(RID):
+    path = '/home/thomas/regional_inversion/input_data/DEMs/RGI60-08/RGI60-08.0'+RID[10] + '/'+RID+ '/dem.tif'
+    dem = rioxr.open_rasterio(path)
+
+    return dem
