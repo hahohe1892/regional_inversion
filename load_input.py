71be6ee0bb73be5f1fa712d9075089837787f79e
G Jake Gebbie <ggebbie@whoi.edu>
2022-09-23 15:05:24 +0200

    load dhdt

---
+++ load_input.py
@@ -0,0 +1,9 @@
+import numpy as np
+import rioxarray as rioxr
+
+RID = 'RGI60-08.00001'
+def load_dhdt(RID):
+    path = '/home/thomas/regional_inversion/input_data/dhdt/RGI60-08/RGI60-08.0'+RID[10] + '/'+RID+ '/dem.tif'
+    dem = rioxr.open_rasterio(path)
+
+    return dhdt
