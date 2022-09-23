4fb520dbef447bd416a7cc4cf3ec7d4b8a35a48d
Thomas Frank <thomas.frank@geo.uu.se>
2022-09-23 17:37:30 +0200

    import mask as xarray

--- load_input.py
+++ load_input.py
@@ -1,39 +1,43 @@
+import numpy as np
 import rioxarray as rioxr
-from oggm import cfg, workflow, tasks, utils
+from oggm import cfg, workflow, utils
 from shapely.geometry import box
 import geopandas as gpd
 
 RID = 'RGI60-08.00010'
-
+glacier_dir = '/home/thomas/regional_inversion/input_data/'
 
 def load_dhdt_path(RID):
-    path = '/home/thomas/regional_inversion/input_data/dhdt/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID+ '/dem.tif'
+    path = glacier_dir + 'dhdt/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID+ '/dem.tif'
     dhdt = rioxr.open_rasterio(path)
 
     return dhdt
 
 
 def load_dem_path(RID):
-    path = '/home/thomas/regional_inversion/input_data/DEMs/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID + '/dem.tif'
+    path = glacier_dir + 'DEMs/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID + '/dem.tif'
     dem = rioxr.open_rasterio(path)
 
     return dem
 
 
 def load_mask_path(RID):
-    path = '/home/thomas/regional_inversion/input_data/dhdt/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID + '/gridded_data.nc'
+    path = glacier_dir + 'dhdt/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID + '/gridded_data.nc'
+    path_tif = glacier_dir + 'DEMs/per_glacier/RGI60-08/RGI60-08.0' + RID[10] + '/'+RID+ '/dem.tif'
     with utils.ncDataset(path) as nc:
         mask = nc.variables['glacier_mask'][:]
+    tif = rioxr.open_rasterio(path_tif)
+    tif.data[0, :, :] = np.copy(mask)
 
-    return mask
+    return tif
 
 
-def crop_border_rio(xarr):
+def crop_border_xarr(xarr, pixels=150):
     res = xarr.rio.resolution()[0]
-    x_min = float(xarr.x.min()) + 150 * res
-    x_max = float(xarr.x.max()) - 150 * res
-    y_min = float(xarr.y.min()) + 150 * res
-    y_max = float(xarr.y.max()) - 150 * res
+    x_min = float(xarr.x.min()) + pixels * res
+    x_max = float(xarr.x.max()) - pixels * res
+    y_min = float(xarr.y.min()) + pixels * res
+    y_max = float(xarr.y.max()) - pixels * res
     geodf = gpd.GeoDataFrame(
         geometry=[
             box(x_min, y_min, x_max, y_max)],
@@ -43,8 +47,8 @@ def crop_border_rio(xarr):
     return clipped
 
 
-def crop_border_arr(arr):
-    return arr[150:-150, 150:-150]
+def crop_border_arr(arr, pixels=150):
+    return arr[pixels:-pixels, pixels:-pixels]
 
 
 def load_dhdt_gdir(RID):
