227205abf1b6322c7c72a95fd2dbc1b6f969fc92
Thomas Frank <thomas.frank@geo.uu.se>
2022-09-23 16:20:19 +0200

    load mask, dem and dhdt

--- get_input.py
+++ get_input.py
@@ -3,7 +3,7 @@ from oggm import utils, workflow, cfg
 import geopandas as gpd
 
 cfg.initialize(logging_level='WARNING')
-cfg.PATHS['working_dir'] = utils.gettempdir(dirname='OGGM_Scandinavia')
+cfg.PATHS['working_dir'] = '~/regional_inversion/input_data/dhdt'#utils.gettempdir(dirname='OGGM_Scandinavia')
 
 fr = utils.get_rgi_region_file('08', version='62')  # Scandinavia
 gdf = gpd.read_file(fr)
