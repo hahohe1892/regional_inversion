637fb2b4e1cb17d109daacad658725d032d3600c
Thomas Frank <thomas.frank@geo.uu.se>
2022-09-30 16:35:32 +0200

    new buffer actually working; working towards functioning regional modelling

--- exec_inversion.py
+++ exec_inversion.py
@@ -6,7 +6,14 @@ import os
 import shutil
 from oggm.core import massbalance
 
-RID = 'RGI60-08.00010'
+#RID = 'RGI60-08.00010'
+#RID = 'RGI60-08.00213' # Storglaciären
+#RID = 'RGI60-08.00006'
+RID = 'RGI60-08.00005'
+glaciers_Sweden = get_RIDs_Sweden()
+RIDs_Sweden = glaciers_Sweden.RGIId
+
+
 working_dir = '/home/thomas/regional_inversion/output/' + RID
 input_file = working_dir + '/input.nc'
 
@@ -42,7 +49,7 @@ options = {
     "-pseudo_plastic_uthreshold": 3.1556926e7,
     "-geometry.update.use_basal_melt_rate": "no",
     "-stress_balance.ssa.compute_surface_gradient_inward": "no",
-    "-flow_law.isothermal_Glen.ice_softness": 3.9565534675428266e-24,
+    "-flow_law.isothermal_Glen.ice_softness": 1.733e3*np.exp(-13.9e4/(8.3*272)), # 3.9565534675428266e-24,
     "-constants.ice.density": 900.,
     "-constants.sea_water.density": 1000.,
     "-bootstrapping.defaults.geothermal_flux": 0.0,
@@ -64,24 +71,32 @@ pism = create_pism(input_file = input_file, options = options, grid_from_options
 
 dh_ref = read_variable(pism.grid(), input_file, 'dhdt', 'm year-1')
 mask = read_variable(pism.grid(), input_file, 'mask', '')
+mask[mask>=0.5] = 1
+mask[mask<0.5] = 0
 S_rec = read_variable(pism.grid(), input_file, 'usurf', 'm')
 B_rec = read_variable(pism.grid(), input_file, 'topg', 'm')
+smb = get_nc_data(input_file, 'climatic_mass_balance', ':')
 
 # set inversion paramters
 dt = .1
-beta = .5
+beta = 1
 theta = 0.05
-bw = 0
-pmax = 10000
+bw = 1
+pmax = 1000
 p_friction = 1000
 max_steps_PISM = 25
 res = dem.rio.resolution()
-A = 3.9565534675428266e-24
+A = 1.733e3*np.exp(-13.9e4/(8.3*272)) # 3.9565534675428266e-24
 
 B_init = np.copy(B_rec)
 S_ref = np.copy(S_rec)
 B_rec_all = []
 misfit_all = []
+
+#k = np.ones((3,3))
+#B_rec = ndimage.convolve(B_rec, k)/9
+#S_rec[B_rec>S_rec] = B_rec[B_rec>S_rec]
+#mask = create_buffer(mask, np.copy(mask), 5)
 
 # do the inversion
 for p in range(pmax):
